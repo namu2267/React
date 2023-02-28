bf16d58653cfc662b4815beca6e1cd01147757e2
namu2267 <namu2267@gmail.com>
2023-02-28 20:37:20 +0900

    useRef(),input에focus주기

--- modern-react/src/InputSample.js
+++ modern-react/src/InputSample.js
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const InputSample = () => {
   const [inputs, setInputs] = useState({
-    name: "",
+    이름: "",
     nickname: "",
   });
 
-  const { name, nickname } = inputs;
+  const nameFocus = useRef();
+
+  const { 이름, nickname } = inputs;
 
   const onChange = (e) => {
     const { value, name } = e.target;
@@ -18,23 +20,30 @@ const InputSample = () => {
 
   const onReset = () => {
     setInputs({
-      name: "",
+      이름: "",
       nickname: "",
     });
+    nameFocus.current.focus();
   };
   return (
     <div>
-      <input name="name" placeholder="이름" onChange={onChange} value={name} />
+      <input
+        name="이름"
+        placeholder="이름쓰세요"
+        onChange={onChange}
+        value={이름}
+        ref={nameFocus}
+      />
       <input
         name="nickname"
-        placeholder="닉네임"
+        placeholder="닉네임쓰세요"
         onChange={onChange}
         value={nickname}
       />
       <button onClick={onReset}>초기화</button>
       <div>
         <b>값:</b>
-        {name}({nickname})
+        {이름}({nickname})
       </div>
     </div>
   );

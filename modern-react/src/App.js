6a7d4bdfe46a681cb1448bacb424a7dc29965590
namu2267 <namu2267@gmail.com>
2023-03-01 18:21:40 +0900

    동적으로 배열렌더링,map()/uuid()사용

--- modern-react/src/App.js
+++ modern-react/src/App.js
@@ -1,9 +1,28 @@
 import React from "react";
 import Counter from "./Counter";
 import InputSample from "./InputSample";
+import UserList from "./UserList";
 
 const App = () => {
-  return <InputSample />;
+  const users리스트관리 = [
+    {
+      id: 1,
+      username: "hihiha2",
+      email: "aaaa@ha.com",
+    },
+    {
+      id: 2,
+      username: "펭수",
+      email: "ppp@ha.com",
+    },
+    {
+      id: 3,
+      username: "짱구",
+      email: "zzang@ha.com",
+    },
+  ];
+
+  return <UserList users={users리스트관리} />;
 };
 
 export default App;
